eb0c0816a93b78b80398d060b413e4bf8f82b3e6
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-11-30 01:12:38 +0530

    Initial commit
    build.sh, shell script to --/build/-- whole "Vitaé®" in one go.
    Usage: $ sh build.sh

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# ====================++== Curriculum Vitaé ==++======================
+#        Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
+#     Copyright © 2006 - 2016, Prabhat Kumar. All rights reserved.
+# ====================================================================
