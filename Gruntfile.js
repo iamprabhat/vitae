84da2f1be2cdc2cced1360511b83e07c6717a4c3
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-11-30 01:22:09 +0530

    Initial commit
    SEED™: Build System — Umeå.
    A Sequømics Product — http://sequomics.com/.

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+
+/*!
+ * Vitaé®
+ * Curriculum Vitaé of Prabhat Kumar — CEO, Founder & Scientist.
+ * ___________________________________________________________________________
+ *
+ * Grunt, http://gruntjs.com/ — The JavaScript Task Runner.
+ * ___________________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ___________________________________________________________________________
+ *
+ * @date      : 30-Nov-2016
+ * @license   : MIT
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt-cli
+ * @build     : SEED™ — Umeå
+ *              └────── A Sequømics Product — http://sequomics.com/.
+ * ___________________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "Vitaé®".
+ * ___________________________________________________________________________
+ */
