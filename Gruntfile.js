218b7f307c8824bd8fc3c5ae2f400ef44103a111
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-12-05 22:09:07 +0530

    Initial commit
    Registering a Grunt task to serve Vitaé® — on to default browser.

--- Gruntfile.js
+++ Gruntfile.js
@@ -180,3 +180,7 @@ module.exports = function(grunt) {
   
   // A task to build JavaScript, also to Lint, concatenate and Minify.
   grunt.registerTask('script', ['jshint', 'concat', 'uglify']);
+  
+  // A task to serve Vitaé®
+  // on to default browser.
+  grunt.registerTask('serve', ['connect', 'watch']);
