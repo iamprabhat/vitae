6565cc413359196706b00b6644dcae7b714bb323
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-12-11 22:35:29 +0530

    Initial commit
    grunt-contrib-csslint — Lint CSS files.
    Usage: $ npm install grunt-contrib-csslint --save-dev | Help: https://github.com/gruntjs/grunt-contrib-csslint

---
+++ tasks/options/csslint.js
@@ -0,0 +1,26 @@
+/*!
+ * Vitaé®
+ * Curriculum Vitaé of Prabhat Kumar — CEO, Founder & Scientist.
+ * ___________________________________________________________________________
+ *
+ * Grunt, http://gruntjs.com/ — The JavaScript Task Runner.
+ * ___________________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ___________________________________________________________________________
+ *
+ * @date      : 11-Dec-2016
+ * @license   : MIT
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt-contrib-csslint
+ * @build     : SEED™ — Umeå
+ *              └────── A Sequømics Product — http://sequomics.com/.
+ * ___________________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "Vitaé®".
+ * ___________________________________________________________________________
+ */
